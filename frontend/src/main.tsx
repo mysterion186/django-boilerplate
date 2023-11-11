4b1ea9f26b1de1580ac99a3bbad5322d7ba4bb02
Mysterion186 <kowsikan@hotmail.fr>
2023-11-11 11:35:41 +0100

    removed unused reference to App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import router from './routes.tsx'
