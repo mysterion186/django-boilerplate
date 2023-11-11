505c6b2fe6412428a1db8df8101d84d9cbfc2575
Mysterion186 <kowsikan@hotmail.fr>
2023-11-11 15:32:12 +0100

    handle (get JWT Token) for basic user too

--- frontend/src/components/authentication/Login.tsx
+++ frontend/src/components/authentication/Login.tsx
@@ -1,10 +1,16 @@
+import { useState, FormEvent, ChangeEvent } from "react";
 import { useGoogleLogin } from "@react-oauth/google";
+
 import AuthApi from "../../services/AuthApi";
 import { ProviderCredentials, RawProviderCredential } from "../../types/api.types";
 import { redirectUser } from "../../utils/AuthUtils";
 import { GoogleButton } from "./SocialButton";
 
 function Login() {
+    const [formData, setFormData] = useState<{username:string, password: string}>({
+        username: '',
+        password: '',
+    });
 
     const LoginFromGoogle = useGoogleLogin({
         onSuccess: async (codeResponse: RawProviderCredential) => {
@@ -21,13 +27,48 @@ function Login() {
         }
     })
 
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        const res = await AuthApi.getJWTToken(formData);
+        redirectUser(res);
+    };
+
+    const handleInputchange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
     return (
         <>
-            <form action="">
-                <input type="text" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputchange}
+                    required
+                    placeholder="username"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputchange}
+                    required
+                    placeholder="password"
+                    />
+                </div>
+                <button type="submit">Login</button>
             </form>
-            <button>Login</button>
             <GoogleButton onClick={LoginFromGoogle}/>
         </>
     )
