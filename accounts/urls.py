890005cd1880b7f1e782b6835a91c58155bcd6e1
Mysterion186 <kowsikan@hotmail.fr>
2023-11-11 15:11:52 +0100

    update urls for generation JWT token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,20 @@
 """urls conf for the accounts app."""
 from django.urls import path, re_path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    re_path('api/register-by-access-token/' + r'social/(?P<backend>[^/]+)/$', views.register_by_access_token),
-    path('api/authentication-test/', views.authentication_test),
+    re_path(
+        'register-by-access-token/' + r'social/(?P<backend>[^/]+)/$',
+        views.register_by_access_token
+    ),
+    path('authentication-test/', views.authentication_test),
+    path('token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
