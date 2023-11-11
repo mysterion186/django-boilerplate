0d50a901e0cf3893600b95bfab29a9c1cbcae7fa
Mysterion186 <kowsikan@hotmail.fr>
2023-11-11 21:46:25 +0100

    set custom user as based user model + add to admin panel

--- backend/settings.py
+++ backend/settings.py
@@ -90,6 +90,8 @@ REST_FRAMEWORK = {
     ]
 }
 
+AUTH_USER_MODEL = "accounts.MyUser"
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
