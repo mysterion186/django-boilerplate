747d27e3b3e5fc21efe3691f59e5e1ccc313a9d5
Mysterion186 <kowsikan@hotmail.fr>
2023-11-16 20:41:27 +0100

    view for displaying user's information

---
+++ frontend/src/components/authentication/User.tsx
@@ -0,0 +1,41 @@
+/**
+ * Quick page in order to display information about the user
+ */
+import { useState, useEffect } from "react";
+import AuthApi from "../../services/AuthApi";
+import AuthStorage from "../../services/AuthStorage";
+import { UserInformation } from "../../types/api.types";
+
+function User() {
+    const [user, setUser] = useState<UserInformation>(
+        {
+            email: "",
+            biography: ""
+        }
+    );
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const token: string = AuthStorage.getJWTToken() as string;
+            const response = await AuthApi.getUserInformation(token);
+
+            if (response.status === 200){
+                setUser(response.data);
+            }
+        }
+        fetchData();
+    }, [])
+
+    return(
+        <>
+            <div>
+                <h1>Congrat's you're logged in !</h1>
+                email: {user.email}
+                <br />
+                biography: {user.biography}
+            </div>
+        </>
+    )
+}
+
+export default User;
