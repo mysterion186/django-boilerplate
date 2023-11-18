a98de85801ddc2a4c82b427e64890e9265494f3f
Mysterion186 <kowsikan@hotmail.fr>
2023-11-18 12:00:56 +0100

    add all links in the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
       <div>
         <h2>React Google Login</h2>
+        <p><Link to={"/login"}>login</Link></p>
+        <p><Link to={"/register"}>register</Link></p>
+        <p><Link to={"/user"}>user</Link></p>
+        <p><Link to={"/update-password"}>update-password</Link></p>
+        <p><Link to={"/password-reset-link"}>password-reset-link</Link></p>
+        <p>Click on the given link for actually change your password</p>
       </div>
   );
 }
