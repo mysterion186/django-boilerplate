9493aed76b69d180571a44590b6d11f48b01161c
Mysterion186 <kowsikan@hotmail.fr>
2023-11-19 22:28:25 +0100

    use a home made function for checking token validity

--- frontend/src/hooks/authentication.ts
+++ frontend/src/hooks/authentication.ts
@@ -4,41 +4,46 @@
 
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { jwtDecrypt } from 'jose';
 import AuthStorage from "../services/AuthStorage";
 
 
-function isTokenValid(token: string): boolean {
-    try {
-      const { payload } = { jwtDecrypt }.decode(token, { complete: true });
-  
-      if (!payload || !payload.exp) {
-        return false; // Invalid token format
-      }
-  
-      const currentTimestamp = Math.floor(Date.now() / 1000);
-      return currentTimestamp < payload.exp;
-    } catch (error) {
-      return false; // Token decoding or verification failed
+function isValideToken (token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    const payload = JSON.parse(jsonPayload);
+    if (payload.exp){
+        const currentTimestamp = Math.floor(Date.now() / 1000);
+
+        // Check if the token has not expired
+        if (payload.exp < currentTimestamp) {
+            console.log('Token has expired');
+            return false; // Token has expired
+        }
+        else{
+            return true;
+        }
     }
+    return false;
 }
 
+// Basic authentication for the user 
 export function useRequireAuth(){
     const navigate = useNavigate();
-
     useEffect(()=>{
         const token: string | null = AuthStorage.getJWTToken();
-        let isAuthenticated:boolean;
+        let isAuthenticated: boolean = false;
         if (typeof token !== 'string'){
             isAuthenticated = false; 
         }
         else {
-            isAuthenticated = isTokenValid(token);
+            isAuthenticated = isValideToken(token);
         }
-    
         if(!isAuthenticated){
-            // navigate("/login");
-            console.log("Please log in !")
+            navigate("/login");
         }
     }, []);
 }
