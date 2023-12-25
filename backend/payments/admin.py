76b989110a8315e3261328327529fe8913dc4470
Mysterion186 <kowsikan@hotmail.fr>
2023-12-25 17:52:40 +0100

    add invoice model

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -1,3 +1,5 @@
+"""Admin for payment app."""
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Invoice)
--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,14 @@
+"""Model for the payment app."""
 from django.db import models
 
-# Create your models here.
+from accounts.models import MyUser
+class Invoice(models.Model):
+    """Model for handling invoices."""
+    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=255)
+    stripe_id = models.CharField(max_length=255)
+    stripe_invoice = models.CharField(max_length=255)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email + " | " + str(self.date)  # pylint: disable=no-member
