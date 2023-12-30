eeb7b0fa866c5efef71ffd85daa6d8f58400036a
Mysterion186 <kowsikan@hotmail.fr>
2023-12-30 21:01:48 +0100

    delete stripe customer id in invoice models (redundant information)

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -5,7 +5,6 @@ from accounts.models import MyUser
 class Invoice(models.Model):
     """Model for handling invoices."""
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    stripe_customer_id = models.CharField(max_length=255)
     stripe_id = models.CharField(max_length=255)
     stripe_invoice = models.CharField(max_length=255)
     date = models.DateField(auto_now=True)
