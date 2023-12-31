9095be7d921d55c52c346dbc17d5b186d1f58804
Mysterion186 <kowsikan@hotmail.fr>
2023-12-31 19:19:59 +0100

    replace print by logger

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,5 +1,7 @@
 """Views for payment application."""
 from typing import Optional, Dict
+import logging
+
 from django.conf import settings
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,6 +11,8 @@ import stripe
 
 from payments.models import Invoice
 from accounts.models import MyUser
+
+logger = logging.getLogger("django")
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class CreateSubscription(APIView):
@@ -31,13 +35,13 @@ class CreateSubscription(APIView):
             }
             if stripe_customer_id:
                 sessions_params["customer"] = stripe_customer_id
-            print(sessions_params)
             checkout_session = stripe.checkout.Session.create(
                 **sessions_params
             )
             return Response({"url":checkout_session.url}, status=status.HTTP_303_SEE_OTHER)
-        except Exception as exc:
-            raise exc
+        except Exception as exc: # pylint: disable=broad-exception-caught
+            logger.error("Something went wrong with subscription creation %s", str(exc))
+            return Response({"error": "something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
 
 class Webhook(APIView):
     """Webhook for handling stripes responses."""
@@ -53,19 +57,24 @@ class Webhook(APIView):
             event = stripe.Webhook.construct_event(
                 payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
             )
-        except ValueError:
-            print("value error")
+        except ValueError as exc:
+            logger.error("There is a value error when listening on the webhook %s", exc)
             return Response({"error": "something went wrong"}, status=status.HTTP_400_BAD_REQUEST)
         except stripe.SignatureVerificationError as exc:
-            raise exc
+            logger.error("Problem with the signature verification %s", exc)
+            return Response(
+                {"error": "There is an issue with the signature verification"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
+        logger.info("Event type %s", event["type"])
+        logger.info("Event object %s", event)
         if event['type'] == 'checkout.session.completed':
             session = event['data']["object"]
             user_id = session.get("client_reference_id")
             stripe_id = session.get("id")
             stripe_customer_id = session.get("customer")
             stripe_invoice = session.get("invoice")
-            print(session)
 
             user = MyUser.objects.get(pk=user_id)
             user.stripe_customer_id = stripe_customer_id
