76f8f3516c767e21107d0a9ce1381b4cfbbaa2e0
Mysterion186 <kowsikan@hotmail.fr>
2023-12-31 19:19:37 +0100

    changed log display format (fullpath + line number instead of module name)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -191,7 +191,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            "format": "{levelname} {asctime} {module} {message}",
+            "format": "{levelname} {asctime} {pathname}:{lineno} {message}",
             "style": "{",
         },
         'simple': {
